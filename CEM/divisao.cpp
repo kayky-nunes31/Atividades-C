6e998fa781635968dbfbdc39173263407790aee5
kayky-nunes31 <kaykynunes357@gmail.com>
2022-05-09 11:25:36 -0300

    Subindo minhas atividades de c++

---
+++ CEM/divisao.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int a,b,r;
+
+int main() {
+
+  cin >> a;
+
+  cin >> b;
+
+  r = a / b;
+
+  cout << r;
+}
---
+++ CEM/entrada_saida.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    string nome;;
+    cout << "Digite seu nome:\n";
+    getline(cin, nome);
+    cout << nome;
+
+    return 0;
+}
